55b14641589eb72703e1fce50ad97ebfccfc4ba7
Allan Brito dos Santos <allan.santos@CONTMATIC.AD>
2019-12-27 19:10:33 -0300

    Aplicada internacionalização na tela de Novo Usuário

--- houseController-controllers/src/main/java/br/com/houseController/controllers/Usuario/NovoUsuarioController.java
+++ houseController-controllers/src/main/java/br/com/houseController/controllers/Usuario/NovoUsuarioController.java
@@ -13,6 +13,7 @@ import br.com.houseController.Exceptions.UsuarioException;
 import br.com.houseController.controllers.ParametrosObjetos;
 import br.com.houseController.controllers.dialogs.Aguarde2;
 import br.com.houseController.controllers.utils.ScreenUtils;
+import br.com.houseController.internationalization.Internationalization;
 import br.com.houseController.model.usuario.Usuario;
 import br.com.houseController.model.usuario.UsuarioLogado;
 import br.com.houseController.service.Usuario.UsuarioService;
@@ -42,12 +43,34 @@ public class NovoUsuarioController extends ParametrosObjetos implements Initiali
 
 	@FXML
 	StackPane spDialog;
+	
+	@FXML
+	Label jlSubTitulo;
+	
+	@FXML
+	Label jlNome;
+	
+	@FXML
+	Label jlLogin;
+	
+	@FXML
+	Label jlSenha;
+	
+	@FXML
+	Label jlEmail;
+	
+	@FXML
+	Label jlSalvar;
+	
+	@FXML
+	Label jlLimpar;
 
 	Usuario usuario = new Usuario();
+	Boolean editar = false;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+		iniciaNomesCampos();
 		listenersCampos();
 
 		Platform.runLater(new Runnable() {
@@ -56,16 +79,26 @@ public class NovoUsuarioController extends ParametrosObjetos implements Initiali
 			public void run() {
 				if (getObjetos() != null) {
 					usuario = (Usuario) getObjetos().get(0);
-					jlTitulo.setText("Editar Usuário");
+					jlTitulo.setText(Internationalization.getMessage("titulo_editar"));
 					jtfNome.setText(usuario.getNome());
 					jtfLogin.setText(usuario.getLogin());
 					jpfSenha.setText("");
 					jtfEmail.setText(usuario.getEmail());
-
+					editar = true;
 				}
 			}
 		});
 
+	}
+
+	private void iniciaNomesCampos() {
+		jlTitulo.setText(Internationalization.getMessage("titulo_novo"));
+		jlSubTitulo.setText(Internationalization.getMessage("msg_preencha_campos"));
+		jlNome.setText(Internationalization.getMessage("campo_nome"));
+		jlLogin.setText(Internationalization.getMessage("campo_login"));
+		jlSenha.setText(Internationalization.getMessage("campo_senha"));
+		jlEmail.setText(Internationalization.getMessage("campo_email"));
+		
 	}
 
 	private void listenersCampos() {
@@ -95,11 +128,11 @@ public class NovoUsuarioController extends ParametrosObjetos implements Initiali
 		    	ScreenUtils.checarCamposVazios(jtfLogin, jpfSenha, jtfNome, jtfEmail);
 				usuario.setAtivo(true);
 				UsuarioService usuarioService = new UsuarioService();
-				if (!jlTitulo.getText().contains("Editar")) {
+				if (!editar) {
 					if (!usuarioService.checaLogin(usuario)) {
 						usuarioService.insert(usuario);
 					} else {
-						throw new UsuarioException("Usuário já cadastrado");
+						throw new UsuarioException(Internationalization.getMessage("erro_usuario_cadastrado"));
 					} 
 				}else{
 					usuarioService.insert(usuario);
@@ -111,15 +144,15 @@ public class NovoUsuarioController extends ParametrosObjetos implements Initiali
 		task.setOnRunning((e) -> Aguarde2.mostrarJanelaAguarde());
 		task.setOnSucceeded((e) -> {
 		    Aguarde2.finalizarJanelaAguarde();				    
-		    if (!jlTitulo.getText().contains("Editar")) {
-		    	ScreenUtils.janelaInformação(spDialog, "Informação", "Usuário cadastrado com sucesso", "Obrigado!");
+		    if (!editar) {
+		    	ScreenUtils.janelaInformação(spDialog, Internationalization.getMessage("header_sucesso1"), Internationalization.getMessage("msg_cadastro_sucesso"), Internationalization.getMessage("certo_button2"));
 		    }else{
-		    	ScreenUtils.janelaInformação(spDialog, "Informação", "Usuário alterado com sucesso", "Obrigado!");
+		    	ScreenUtils.janelaInformação(spDialog, Internationalization.getMessage("header_sucesso1"), Internationalization.getMessage("msg_alterado_sucesso"), Internationalization.getMessage("certo_button2"));
 		    }
 		});
 		task.setOnFailed((e) -> {
 			Aguarde2.finalizarJanelaAguarde();
-			ScreenUtils.janelaInformação(spDialog, "Vixi", e.getSource().getException().getMessage(), "Tudo bem!");
+			ScreenUtils.janelaInformação(spDialog, Internationalization.getMessage("header_erro2"), e.getSource().getException().getMessage(),  Internationalization.getMessage("erro_button1"));
 		});
 		new Thread(task).start();		
 		
